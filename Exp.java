e761ce35b3a33b9ace64696e4b670de51e991a74
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-05-23 12:48:32 -0400

    Update Exp.java

--- Exp.java
+++ Exp.java
@@ -16,34 +16,31 @@ public class Exp {
      */
     public static void main(String[] args) {
         int n = 100;
-        int x =3;
+        double x =3;
         System.out.println("e  cua x=3 va n =100 la "+taylor(n,x));
     }
-    public static double taylor (int n, int x){
-    double exp=0;
-    for(int i=0;i<n;i++){
-    exp=exp+emp(i,x);
-    
-    }//end for loop tinh e mu x
-    return exp;}//end exp method
-    public static double emp (int soTu, int x){
-    
-    double emphasis=0;
-    double emphasisBottom=1;
-    double emphasisTop=1;
-    if(soTu==0){
-    emphasisBottom=1;
-    emphasisTop=1;
-    emphasis=emphasisTop/emphasisBottom;
-    return emphasis;
-    }//end if
-    for (int z=1;z<soTu;z++){
-    emphasisBottom=emphasisBottom*z;
-    }//end for mau so
-    for (int z=1;z<soTu;z++){
-    emphasisTop*=x;
-    }//end for tu so
-    emphasis=emphasisTop/emphasisBottom;//emp = tu/mau
-    return emphasis; }//end emp method
-    
+    public static double taylor (int n, double x){
+        double exp=0;
+        for(int i =0;i<=n;i++ ){
+            exp+=emp(x,i);
+        }//end for
+        return exp;
+    }//end exp
+    public static double emp(double x,int i){
+        double emphasis=0;
+        double top=1;
+        double bottom=1;
+        top = Math.pow(x,i);
+        if(i==0){
+            bottom=1;
+        }//end if
+        for (int j=1;j<=i;j++){
+            bottom=bottom*j;
+        }//end for
+        emphasis=top/bottom;
+        return emphasis;
+    }//end emp
 }
+    
+
+
