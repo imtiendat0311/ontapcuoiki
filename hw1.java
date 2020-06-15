15018055f756dbed7d97d3e3289d6f3d6778cb45
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-06-15 23:17:45 +0700

    Update hw1.java

--- hw1.java
+++ hw1.java
@@ -5,19 +5,19 @@ public class hw1{
     public static void main(String[] args) {
 int n ;
 String temp;
-Scanner s = new Scanner(system.in);
+Scanner s = new Scanner(System.in);
 n=s.nextInt();
-Scanner s1 = new Scanner(system.in);
+Scanner s1 = new Scanner(System.in);
 String names[]= new String[n];
 for(int i =0; i<n ; i++){
- name[i]=s1.nextLine();   
+ names[i]=s1.nextLine();   
 }
 for(int i=0;i<n;i++){
     for(int j=i+1;j<n;j++){
         if(names[i].compareTo(names[j])>0){
             temp = names[i];
             names[i]=names[j];
-            names[j]=names[i];
+            names[j]=temp;
         }
     }
 }
