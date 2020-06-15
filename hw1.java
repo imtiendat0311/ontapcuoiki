4c81c8103e40e057c98bf2d1c351b194defea570
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-06-15 23:06:14 +0700

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class hw1{
+
+    public static void main(String[] args) {
+int n ;
+String temp;
+Scanner s = new Scanner(system.in);
+n=s.nextInt();
+Scanner s1 = new Scanner(system.in);
+String names[]= new String[n];
+for(int i =0; i<n ; i++){
+ name[i]=s1.nextLine();   
+}
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if(names[i].compareTo(names[j])>0){
+            temp = names[i];
+            names[i]=names[j];
+            names[j]=names[i];
+        }
+    }
+}
+System.out.println("Names sorted order :");
+for(int i =0; i<n-1;i++){
+    System.out.println(names[i]);
+}
+System.out.println(names[n-1]);
+}
+
+}
