e9f07d32452cd97e23804090c730bea90a013492
imtiendat0311 <60676245+imtiendat0311@users.noreply.github.com>
2020-10-15 09:19:02 +0700

    missing 8

--- baiTap.java
+++ baiTap.java
@@ -1,9 +1,10 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
 public class baiTap {
     public static void main(String args[]){
       //  System.out.println(baCanhTamGiac(2,3,4));
-        tinhoddeven();
+        daysoFib();
     }
     //bai 1
     public static double baCanhTamGiac(int a ,int b, int c){
@@ -283,6 +284,21 @@ public class baiTap {
         System.out.println(hex);
 
     }
+
+    //bai 17
+    public static void luyThua(){
+        Scanner input=new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        //dung loop
+        int sum=1;
+        for(int i=0;i<y;i++){
+            sum=sum*x;
+        }
+        System.out.println(sum);
+        //hoac dung Math.pow(x,y);
+    }
+
     //bai18
     public static void tinhToan(){
         Scanner input = new Scanner(System.in);
@@ -351,6 +367,45 @@ public class baiTap {
                 }
         }
     }
+    //bai22
+    public static void soHanhPhuc(){
+        int[]a=new int[]{0,0,0,0,0,0};
+        Scanner input = new Scanner(System.in);
+        int number=input.nextInt();
+        int n = number;
+        int i=5;
+        while(number!=0) {
+            a[i] = number % 10;
+            number = number / 10;
+            i--;
+        }
+        int sum=0;
+        for(int k=0;k<a.length/2;k++){
+            sum=sum+(a[k]-a[(a.length-1)-k]);
+        }
+        if(sum==0){
+            System.out.print(n+" la so hanh phuc");
+        }
+    }
+    //bai23
+    public static void timsomersen(){
+        Scanner input = new Scanner(System.in);
+       int n=input.nextInt();
+        for(int i=2;i<=n;i++){
+            int j=2;
+            int count=0;
+            while(j<=i/2){
+                if(i%j==0){
+                    count++;
+                }
+                j++;
+            }
+            double a =Math.pow(2,i)-1;
+            if(count==0&&a<=n) {
+            System.out.println(a);
+            }
+        }
+    }
     //bai25
     public static void tinhoddeven(){
         Scanner input= new Scanner(System.in);
@@ -370,6 +425,113 @@ public class baiTap {
                 multi=multi*i;
             }
             System.out.println(multi);
+        }
+    }
+    //bai24  da tung lam
+    
+    //bai26
+    public static void daysoFib() {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        long m = input.nextLong();
+        long n2=1,n1=1,n3 = 0;
+        //so fib thu n
+        System.out.print("So Fib thu "+n+" : ");
+        if(n==1||n==2){
+            System.out.println(n2);
+        }
+        else{
+        for(int i=3;i<=n;i++){
+            n3=n2+n1;
+            n1=n2;
+            n2=n3;
+        }
+        System.out.println(n3);
+        }
+
+        //cac so fib be hon m
+        long n4=0;long n5=1,n6=1,k=0;
+        System.out.print("Cac so Fib be hon "+m+" : ");
+        while(n4<m) {
+            if (k == 0 || k == 1) {
+                System.out.print("1 ");
+                k++;
+            } else {
+                n4 = n5 + n6;
+                n5 = n6;
+                n6 = n4;
+                if (n4 > m) {
+                    break;
+                }
+                System.out.print(n4 + " ");
+                k++;
+            }
+        }
+
+        //tim tong m so fib
+        System.out.println();
+        long n7=1,n8=1,n9=0,sum=0,l=0;
+        System.out.print("tong "+m+" so fib : ");
+        while(m>0){
+        if(l==0||l==1){
+            sum=sum+n7;
+            l++;
+            m--;
+        }
+        else{
+            n9=n7+n8;
+            sum=sum+n9;
+            n7=n8;
+            n8=n9;
+            m--;
+        }
+        }
+
+        //check so fib n
+        System.out.print(sum);
+        System.out.println();
+        long n10=1,n11=1,n12=0;
+        boolean flag=false;
+        for(int v=0;n12<n;v++){
+            if(n==1){
+                System.out.println(n+" is the fib number ");
+            }
+            else{
+                n12=n10+n11;
+                n10=n11;
+                n11=n12;
+                if(n12==n){
+                    flag=true;
+                    break;
+                }
+            }
+        }
+        if(flag){
+            System.out.println(n+" is the fib number");
+        }
+        else{
+            System.out.println(n+" isn't the fib number");
+        }
+
+        //phan tach n thanh tong fib
+        long n13=1,n14=1,n15=0;
+        System.out.print(n+" la tong cua cac so fib sau : ");
+        if(n==1){
+            System.out.println(n13);
+        }
+        while(n>0) {
+            for (int c = 0; n15 < n; c++) {
+                n15 = n13 + n14;
+                if(n15>n){
+                    n15=n14;
+                    break;}
+                n13 = n14;
+                n14 = n15;
+
+            }
+            n=n-n15;
+            System.out.print(n15+" ");
+            n15=0;n14=1;n13=1;
         }
     }
 
